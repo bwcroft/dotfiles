23d16e808c76e12328840e70b844086faadfb661
Brenden Croft <brenden848@gmail.com>
2024-02-21 22:54:37 -0700

    refactor: updated nvim and brew packages

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,39 +1,58 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    lazy = false,
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    lazy = false,
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+					"jsonls",
+					"html",
+					"cssls",
+					"dotls",
+					"gopls",
+					"marksman",
+					"intelephense",
+					"pylsp",
+					"rust_analyzer",
+					"vuels",
+					"dockerls",
+					"docker_compose_language_service",
+					"bashls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
-        capabilities = capabilities
-      })
-      lspconfig.html.setup({
-        capabilities = capabilities
-      })
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
-      })
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.dotls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.intelephense.setup({})
+			lspconfig.pylsp.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.vuels.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.docker_compose_language_service.setup({})
+			lspconfig.bashls.setup({})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
