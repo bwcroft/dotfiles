b49fe685399a9eabfd5110544706bbaae5fafe46
Brenden Croft <brenden848@gmail.com>
2024-03-03 19:06:42 -0700

    chore: updated theme colors

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,39 +1,38 @@
 return {
-	{
-		"marko-cerovac/material.nvim",
-		config = function()
-			require("material").setup({
-				contrast = {
-					sidebars = false,
-					floating_windows = true,
-					cursor_line = false,
-					non_current_windows = false,
-					filetypes = {},
-				},
-				plugins = {
-					"neo-tree",
-					"telescope",
-				},
-				disable = {
-					colored_cursor = false,
-					borders = false,
-					background = false,
-					term_colors = false,
-					eob_lines = true,
-				},
-				lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
-				async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
-				custom_colors = function(colors)
-          colors.syntax.variable = "#FFFFFF"
-          colors.syntax.type = "#FFCB6B"
-				end,
-        custom_highlites = {
-          Statement = { fg = "#000000" } 
+  {
+    "marko-cerovac/material.nvim",
+    config = function()
+      require("material").setup({
+        contrast = {
+          sidebars = false,
+          floating_windows = true,
+          cursor_line = false,
+          non_current_windows = false,
+          filetypes = {},
         },
-			})
+        plugins = {
+          "neo-tree",
+          "telescope",
+        },
+        disable = {
+          colored_cursor = false,
+          borders = false,
+          background = false,
+          term_colors = false,
+          eob_lines = true,
+        },
+        lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+        async_loading = true,  -- Load parts of the theme asyncronously for faster startup (turned on by default)
+        custom_colors = function(colors)
+          colors.editor.fg_dark = "#F6F6F6"
+          colors.syntax.variable = "#FFFFFF"
+          colors.syntax.value = colors.main.purple
+          colors.syntax.field = colors.main.purple
+        end,
+      })
 
-			vim.g.material_style = "darker"
-			vim.cmd.colorscheme("material")
-		end,
-	},
+      vim.g.material_style = "darker"
+      vim.cmd.colorscheme("material")
+    end,
+  },
 }
