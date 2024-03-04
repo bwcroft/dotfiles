ea2de74bc84cb3c2f252e64c345fe02463c17414
Brenden Croft <brenden848@gmail.com>
2024-03-03 17:38:11 -0700

    init material theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,34 +1,39 @@
 return {
-  {
-    "marko-cerovac/material.nvim",
-    config = function()
-      require("material").setup({
-        contrast = {
-          terminal = false,
-          sidebars = false,
-          floating_windows = true,
-          cursor_line = false,
-          non_current_windows = false,
-          filetypes = {},
+	{
+		"marko-cerovac/material.nvim",
+		config = function()
+			require("material").setup({
+				contrast = {
+					sidebars = false,
+					floating_windows = true,
+					cursor_line = false,
+					non_current_windows = false,
+					filetypes = {},
+				},
+				plugins = {
+					"neo-tree",
+					"telescope",
+				},
+				disable = {
+					colored_cursor = false,
+					borders = false,
+					background = false,
+					term_colors = false,
+					eob_lines = true,
+				},
+				lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
+				async_loading = true, -- Load parts of the theme asyncronously for faster startup (turned on by default)
+				custom_colors = function(colors)
+          colors.syntax.variable = "#FFFFFF"
+          colors.syntax.type = "#FFCB6B"
+				end,
+        custom_highlites = {
+          Statement = { fg = "#000000" } 
         },
-        plugins = {
-          "neo-tree",
-          "telescope",
-        },
-        disable = {
-          colored_cursor = false,
-          borders = false,
-          background = false,
-          term_colors = false,
-          eob_lines = true,
-        },
-        lualine_style = "default", -- Lualine style ( can be 'stealth' or 'default' )
-        async_loading = true,  -- Load parts of the theme asyncronously for faster startup (turned on by default)
-        custom_colors = nil,   -- If you want to override the default colors, set this to a function
-      })
+			})
 
-      vim.g.material_style = "darker"
-      vim.cmd.colorscheme("material")
-    end,
-  },
+			vim.g.material_style = "darker"
+			vim.cmd.colorscheme("material")
+		end,
+	},
 }
