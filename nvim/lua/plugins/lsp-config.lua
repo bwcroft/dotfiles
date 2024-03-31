e74c297c47667387d0971861db8cec883a1084a2
Brenden Croft <brenden848@gmail.com>
2024-03-30 23:32:28 -0600

    feat: added lsp_lines

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -1,68 +1,78 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = {
-          "lua_ls",
-          "eslint",
-          "tsserver",
-          "jsonls",
-          "html",
-          "cssls",
-          "dotls",
-          "gopls",
-          "graphql",
-          "marksman",
-          "intelephense",
-          "pylsp",
-          "rust_analyzer",
-          "vuels",
-          "dockerls",
-          "docker_compose_language_service",
-          "bashls",
-        },
-      })
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"eslint",
+					"tsserver",
+					"jsonls",
+					"html",
+					"cssls",
+					"dotls",
+					"gopls",
+					"graphql",
+					"marksman",
+					"intelephense",
+					"pylsp",
+					"rust_analyzer",
+					"vuels",
+					"dockerls",
+					"docker_compose_language_service",
+					"bashls",
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-      lspconfig.lua_ls.setup({})
-      lspconfig.tsserver.setup({
-        init_options = {
-          preferences = {
-            disableSuggestions = true,
-          },
-        },
-      })
-      lspconfig.eslint.setup({})
-      lspconfig.jsonls.setup({})
-      lspconfig.html.setup({})
-      lspconfig.cssls.setup({})
-      lspconfig.dotls.setup({})
-      lspconfig.gopls.setup({})
-      lspconfig.graphql.setup({})
-      lspconfig.marksman.setup({})
-      lspconfig.intelephense.setup({})
-      lspconfig.pylsp.setup({})
-      lspconfig.rust_analyzer.setup({})
-      lspconfig.vuels.setup({})
-      lspconfig.dockerls.setup({})
-      lspconfig.docker_compose_language_service.setup({})
-      lspconfig.bashls.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({
+				init_options = {
+					preferences = {
+						disableSuggestions = true,
+					},
+				},
+			})
+			lspconfig.eslint.setup({})
+			lspconfig.jsonls.setup({})
+			lspconfig.html.setup({})
+			lspconfig.cssls.setup({})
+			lspconfig.dotls.setup({})
+			lspconfig.gopls.setup({})
+			lspconfig.graphql.setup({})
+			lspconfig.marksman.setup({})
+			lspconfig.intelephense.setup({})
+			lspconfig.pylsp.setup({})
+			lspconfig.rust_analyzer.setup({})
+			lspconfig.vuels.setup({})
+			lspconfig.dockerls.setup({})
+			lspconfig.docker_compose_language_service.setup({})
+			lspconfig.bashls.setup({})
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+	{
+		"https://github.com/bwcroft/lsp_lines.nvim",
+		config = function()
+			require("lsp_lines").setup()
+			-- Disable virtual_text since it's redundant due to lsp_lines.
+			vim.diagnostic.config({
+				virtual_text = false,
+			})
+		end,
+	},
 }
