b591c3001f83b83a890f123ceed0adb87eef78b2
Brenden Croft <brenden.croft@clearlink.com>
2024-11-26 21:33:03 -0700

    chore: added startup logs and theme config updates

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,39 +1,33 @@
 return {
-  "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
+	"goolord/alpha-nvim",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local alpha = require("alpha")
+		local dashboard = require("alpha.themes.dashboard")
 
-  config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.dashboard")
+		dashboard.section.header.val = {
+      [[                                                       ]],
+      [[                                                       ]],
+			[[   ███    ██ ███████  ██████  ██    ██ ██ ███    ███   ]],
+			[[   ████   ██ ██      ██    ██ ██    ██ ██ ████  ████   ]],
+			[[   ██ ██  ██ █████   ██    ██ ██    ██ ██ ██ ████ ██   ]],
+			[[   ██  ██ ██ ██      ██    ██  ██  ██  ██ ██  ██  ██   ]],
+			[[   ██   ████ ███████  ██████    ████   ██ ██      ██   ]],
+      [[                                                       ]],
+      [[                                                       ]],
+		}
 
-    dashboard.section.header.val = {
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                     ]],
-      [[       ████ ██████           █████      ██                     ]],
-      [[      ███████████             █████                             ]],
-      [[      █████████ ███████████████████ ███   ███████████   ]],
-      [[     █████████  ███    █████████████ █████ ██████████████   ]],
-      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-      [[                                                                       ]],
-    }
 
-    dashboard.section.buttons.val = {
-      dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("spc ff", "  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("spc fg", "  Find text", ":Telescope live_grep <CR>"),
-      dashboard.button("spc e", " Toggle File Tree", ":NvimTreeToggle<CR>"),
-      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
-    }
+		dashboard.section.buttons.val = {
+			dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
+			dashboard.button("spc ff", "  Find file", ":Telescope find_files <CR>"),
+			dashboard.button("spc fg", "  Find text", ":Telescope live_grep <CR>"),
+			dashboard.button("spc e", " Toggle File Tree", ":NvimTreeToggle<CR>"),
+			dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+		}
 
-    alpha.setup(dashboard.opts)
-  end,
+		alpha.setup(dashboard.opts)
+	end,
 }
