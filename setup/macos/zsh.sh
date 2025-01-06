9fcfd2894ccf18dfb6948cd0b884a787baaa3b35
Brenden Croft <brenden848@gmail.com>
2025-01-05 19:57:42 -0700

    chore: removed p10k and replaced with oh-my-posh

--- setup/macos/zsh.sh
+++ setup/macos/zsh.sh
@@ -4,8 +4,6 @@ source "$(dirname "$0")/../logger.sh"
 
 setup_oh_my_zsh() {
   local omz="oh-my-zsh"
-  local p10k="powerlevel10k"
-  local p10kPath="$HOME/.oh-my-zsh/custom/themes/powerlevel10k"
 
   if [ ! -d "$HOME/.oh-my-zsh" ]; then
     log_install "$omz"
@@ -14,19 +12,6 @@ setup_oh_my_zsh() {
   else 
     log_exists "$omz"
   fi
-
-  if [ ! -d "$p10kPath" ]; then
-    log_install "$p10k"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$p10kPath"
-    log_installed "$p10k"
-  else
-    log_exists "$p10k"
-  fi
-
-  log_setup_start "$p10k"
-  rm -rf $HOME/.p10k.zsh
-  ln -s "$HOME/.dotfiles/zsh/p10k.zsh" "$HOME/.p10k.zsh"
-  log_setup_complete "$p10k"
 }
 
 setup_zsh() {
