ae00a77cfd288baed94cd87320d0a5f371407b5c
Brenden Croft <brenden848@gmail.com>
2025-01-06 22:54:24 -0700

    chore: replaced oh-my-posh with starship

--- setup/macos/zsh.sh
+++ setup/macos/zsh.sh
@@ -4,14 +4,19 @@ source "$(dirname "$0")/../logger.sh"
 
 setup_oh_my_zsh() {
   local omz="oh-my-zsh"
-
-  if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  local starshipPath="$HOME/.config/starship.toml"
+  if [ ! -d "$HOME/.$omz" ]; then
     log_install "$omz"
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     log_installed "$omz"
   else 
     log_exists "$omz"
   fi
+
+  log_setup_start "starship"
+  rm -rf "$starshipPath"
+  ln -s "$HOME/.dotfiles/zsh/starship.toml" "$starshipPath"
+  log_setup_complete "starship"
 }
 
 setup_zsh() {
