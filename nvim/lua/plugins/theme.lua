b4aa0d691c72acf80200b1155de752f14206ff26
Brenden Croft <brenden848@gmail.com>
2025-07-28 11:07:35 -0600

    fix: fixed lsp info window not having padding and boarder

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,44 +1,34 @@
 return {
-  {
-    "catppuccin/nvim",
-    lazy = false,
-    name = "catppuccin",
-    priority = 1000,
-    config = function()
-      -- Configure Theme
-      require("catppuccin").setup({
-        term_colors = false,
-        styles = {
-          comments = {},
-          conditionals = {},
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-        },
-        color_overrides = {
-          mocha = {
-            base = "#000000",
-            mantle = "#000000",
-            crust = "#000000",
-          },
-        },
-      })
-
-      -- Apply Theme
-      vim.cmd.colorscheme("catppuccin")
-
-      -- Add Border & Padding to Lsp Hover
-      local hoverConfig = vim.lsp.with(vim.lsp.handlers.hover, {
-        border = "single",
-        focusable = false,
-      })
-      vim.lsp.handlers["textDocument/hover"] = hoverConfig
-    end,
-  },
+	{
+		"catppuccin/nvim",
+		lazy = false,
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
+				term_colors = false,
+				styles = {
+					comments = {},
+					conditionals = {},
+					loops = {},
+					functions = {},
+					keywords = {},
+					strings = {},
+					variables = {},
+					numbers = {},
+					booleans = {},
+					properties = {},
+					types = {},
+				},
+				color_overrides = {
+					mocha = {
+						base = "#000000",
+						mantle = "#000000",
+						crust = "#000000",
+					},
+				},
+			})
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 }
