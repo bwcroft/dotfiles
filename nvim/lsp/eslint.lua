8b7fe75c76bc88c65206078814e643ab879fb612
Brenden Croft <brenden848@gmail.com>
2026-03-07 18:43:24 -0700

    chore(eslint.lsp): fix root dir detection to not check for package.json

--- nvim/lsp/eslint.lua
+++
@@ -1,9 +0,0 @@
-return {
-  root_dir = nil,
-  root_markers = { "eslint.config.js", ".git" },
-  settings = {
-    experimental = {
-      useFlatConfig = true,
-    },
-  },
-}
--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -73,6 +73,23 @@ return {
 				},
 			})
 
+			vim.lsp.config("eslint", {
+				root_dir = function(bufnr, on_dir)
+					local root = vim.fs.root(bufnr, {
+						".eslintrc",
+						".eslintrc.js",
+						".eslintrc.cjs",
+						".eslintrc.json",
+						"package.json",
+						".git",
+					})
+
+					if root then
+						on_dir(root)
+					end
+				end,
+			})
+
 			vim.lsp.enable({
 				"lua_ls",
 				"vtsls",
